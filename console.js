a8558287279d03cb6aabb9e6812680942f610e3f
Preizt <prieztly.tennysson@gmail.com>
2024-12-15 21:16:21 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,162 @@
+/*Write  a program to check whether a number is plaindrome or not?*/
+
+/*Write a program to  find numbers which are multiples of  both 5 and 25 between 1000 and 1500*/
+
+// let i;
+
+// for(i=1000;i<=1500;i++)
+// {
+//     if(i%5==0 && i%25==0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+/*Find the sum of even numbers within a range.*/
+
+// var i;
+// var sum=0
+
+// for(i=1;i<=10;i++)
+// {
+//     if(i%2==0){
+      
+//         sum=sum+i
+
+//     }
+// }
+
+// console.log(sum);
+
+
+/*Find the Factorial of a Number*/
+
+// let number = 10;
+// let factorial = 1;
+
+// for(i=1;i<=number;i++)
+// {
+//     factorial=factorial*i
+// }
+
+// console.log(factorial);
+
+
+/*Write a program which prints the multiples of 3 in between 0-100*/
+
+
+// let i;
+
+// for(i=0;i<=100;i++)
+// {
+//     if(i%3==0){
+//         console.log(i);
+        
+//     }
+// }
+
+
+
+/*Calculate the sum of numbers from 1 to 10 using a loop.*/
+
+// let i;
+// let sum=0;
+
+// for(i=1;i<=10;i++)
+// {
+//    sum=sum+i;
+// }
+
+// console.log(sum);
+
+
+/*Write a program to print first 10 fibnocci numbers*/
+
+/*Write a program to find the largest element in a nested array.*/
+
+/*Display multiplication table of 8 using JS. ( 8*1=8 to 8*10=80)*/
+
+// for (let i = 1; i <= 10; i++) 
+//     { 
+        
+//         console.log(`8 * ${i} = ${8 * i}`); 
+
+// }
+
+
+/*Find the sum of even numbers from 200 to 500.*/
+
+// let i;
+// let sum=0;
+
+// for(i=200;i<=500;i++)
+// {
+//     if(i%2==0){
+        
+//         sum=sum+i;
+
+//     }
+// }
+
+// console.log(sum);
+
+
+/*Print Odd Numbers between 1 and 50*/
+
+// let i;
+
+// for(i=1;i<=50;i++)
+// {
+//     if(i%2==1)
+//     {
+//         console.log(i);
+        
+//     }
+// }
+
+/*Create a resume for yourself using a JavaScript program.*/
+
+
+
+
+
+
+/*Create an object in JavaScript using two arrays, where the first array contains 5 keys (e.g., 'name', 'age', etc.) and the second array contains 5 corresponding values (e.g., 'Sahad', '24', etc.), by iterating through the arrays with a for loop.*/
+
+
+
+// const obj = {};
+
+// let a1 = ["Name", "Age", "Gender", "DOB", "Place"];
+// let a2 = ["Preizt", "24", "Male", "29-12-1999", "Kollam"];
+// for (let i = 0; i < 5; i++) {
+//   obj[a1[i]] = a2[i];
+// }
+// console.log(obj);
+
+
+
+
+/*Write a JavaScript program that prints all the even numbers from 1 to 100 using looping statement*/
+
+// let i;
+
+// for(i=1;i<=100;i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
+        
+//     }
+// }
+
+
+/*Print prime numbers between 1 and 100*/
+
+
+
+
+
+
+
